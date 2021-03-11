42e021e3d767619f9cb6fc9c8d7387d8d0ffea98
Jonathan Souza <jonathancsouza5@gmail.com>
2021-03-11 14:45:57 -0800

    Add project files.

---
+++ SubWords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SubWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
